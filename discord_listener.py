00a6f048027a1771c478584aae97071d3ca71a45
vzelinsky <vzelinsky@albany.edu>
2019-06-13 16:42:40 -0400

    Updated functionality for discord_listener.py and gsheets_updater.py. Bot now has the functionality to add a user. The gsheets_updater function get_next_empty_cell_after_cell is a more robust and finds an empty cell next to (after column-wise) a specified cell.

--- discord_listener.py
+++ discord_listener.py
@@ -1,6 +1,8 @@
 import discord
 import json
-from points_parser import s_parse_add_points
+from discord_command_parser import *
+from gsheets_updater import *
+from gspread import *
 
 config_fn = "config.json"
 try:
@@ -10,10 +12,11 @@ except IOError:
     print("ERROR: {0} could not be opened.".format(config_fn))
     exit()
 except KeyError:
-    print("ERROR: config.json did not contain a discord token")
+    print("ERROR: {0} did not contain a discord token".format(config_fn))
     exit()
 
 client = discord.Client()
+sh = get_spreadsheet("Points Logger")
 
 @client.event
 async def on_ready():
@@ -24,17 +27,30 @@ async def on_message(message):
     # disregard messages sent by the bot
     if message.author == client.user:
         return
+
+    wk = get_worksheet(sh, get_current_month_name())
     
     if message.content.startswith('!add points'):
         try:
-            author = message.author.display_name
-            points = s_parse_add_points(message.content)
+            author = message.author.name
+            points = parse_add_points(message.content)
         except Exception as e:
             print(e)
             await message.add_reaction("❌")
             return
-        print(author)
-        print(points)
+        print("Gave {0} points to {1}.".format(points, author))
         await message.add_reaction("✅")
+    elif message.content.startswith("!add me"):
+        try:
+            author = message.author.name
+            parse_add_me(message.content)
+            add_user(wk, author)
+        except Exception as e:
+            print(e)
+            await message.add_reaction("❌")
+            return
+        print("{0} added successfully.".format(author))
+        await message.add_reaction("✅")
+
     
 client.run(discord_bot_token)
--- gsheets_updater.py
+++ gsheets_updater.py
@@ -1,51 +1,70 @@
-import gspread
+from typing import Any
+from gspread import *
 from gspread.utils import rowcol_to_a1
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime as dt
 
 
-def get_spreadsheet(name):
+def get_spreadsheet(name: str) -> Spreadsheet:
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name("google_config.json", scope)
-    gc = gspread.authorize(creds)
+    gc = authorize(creds)
     sh = gc.open(name)
     return sh
 
 
-def get_worksheet(spreadsheet, name):
+def get_worksheet(spreadsheet: Spreadsheet, name: str) -> Worksheet:
+    # TODO : need to handle exception here?
     return spreadsheet.worksheet(name)
 
 
-def get_current_month():
+def get_current_month_name() -> str:
     return dt.now().strftime("%B")
 
 
-def get_next_empty_name_cell_a1(worksheet):
-    row = worksheet.find("Names").row
-    all_names = worksheet.row_values(row)[1:]
-    col_counter = 2
-    empty_cell = None
+def get_next_empty_cell_after_cell(worksheet: Worksheet, after_cell: Cell, return_format="a1") -> Any:
+    """ Gets the next empty cell after a specified cell (column-wise) in the specified format """
+    # look at all cells next to after_cell
+    all_names = worksheet.row_values(after_cell.row)[after_cell.col:]
+    # start col counter at cell next to after_cell
+    col_counter = after_cell.col + 1 
     for name in all_names:
-        if not name: # If string is empty
+        # if string is empty we've found an empty cell inbetween names
+        if not name:
             break
         col_counter += 1
+    # if no empty cells found inbetween names the col_counter will be at cell after all names 
     try:
-        empty_cell = worksheet.cell(row, col_counter)
-    except Exception:
+        empty_cell = worksheet.cell(after_cell.row, col_counter)
+    # if we encounter an APIError we've run out of columns and need to allocate more
+    except GSpreadException: 
         worksheet.add_cols(10)
-        empty_cell = worksheet.cell(row, col_counter)
-    return rowcol_to_a1(empty_cell.row, empty_cell.col)
+        empty_cell = worksheet.cell(after_cell.row, col_counter)
+    # return cell based on return format
+    if 'a1' in return_format:
+        return_val = rowcol_to_a1(empty_cell.row, empty_cell.col)
+    elif 'cell' in return_format:
+        return_val = empty_cell
+    elif 'rowcol' in return_format:
+        return_val = empty_cell.row, empty_cell.col
+    else:
+        raise Exception("ERROR: Unknown format requested : '{0}'. Expected one of : 'a1', 'cell', 'rowcol'.".format(return_format))
+    return return_val
 
 
+def add_user(worksheet: Worksheet, username: str) -> str:
+    try: 
+        worksheet.find(username)
+    except CellNotFound:
+        names_cell = worksheet.find("Names")
+        empty_cell_a1 = get_next_empty_cell_after_cell(worksheet, names_cell, return_format="a1")
+        worksheet.update_acell(empty_cell_a1, username)
+    else:
+        raise Exception("ERROR: User '{0}' already exists.".format(username))
 
-def add_user(worksheet, username):
-    empty_cell_a1 = get_next_empty_name_cell_a1(worksheet)
-    worksheet.update_acell(empty_cell_a1, username)
 
+# points_logger_sheet = get_spreadsheet("Points Logger")
+# current_month = get_current_month_name()
+# current_worksheet = get_worksheet(points_logger_sheet, current_month)
 
-
-points_logger_sheet = get_spreadsheet("Points Logger")
-current_month = get_current_month()
-current_worksheet = get_worksheet(points_logger_sheet, current_month)
-
-add_user(current_worksheet, "test")
+# print(add_user(current_worksheet, "boop"))
